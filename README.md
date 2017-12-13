74aaee703967e131d47c7d9197f505ae9d70b592
Gagankaurgill <33652333+Gagankaurgill@users.noreply.github.com>
2017-12-13 08:34:28 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Public Library Management system
+
+A basic public library mangement system web app based on nodejs express framework.
+Uses Handlebars as templating engine.
+Uses MongoDB as the DB backend.
+Uses Bootstrap-Node js as front-end
+
+# Pre-Requisite
+
+ MongoDB on localhost:27017 (default address) If using local mongodb instance.
+ 
+# Steps to configure
+
+Open Command Prompt and navigate to the folder.
+Run command npm install followed by bower install bootstrap
+
+After successfull installation of both node modules and bower components, 
+type npm start
+
+# Accesing the web app
+
+After typing in npm start, Open Browser and type
+localhost:3000 to access the web app.
+
+Using following existing creds to log in to the app-
+Email- admin@plm.com
+Password- admin
---
+++ routes/index.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'danish' });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    email: req.body.inputEmail,
+    password: req.body.inputPassword
+    
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/login');
+});
+
+router.post('/userdata', function(req, res, next) {
+  var email=req.body.inputEmail;
+  var password=req.body.inputPassword;
+  if (email=="admin@plm.com" && password=="admin"){
+    res.redirect('/home');
+  }else{
+    res.redirect('/login');
+  }
+
+  
+});
+
+router.post('/insertLibrary', function(req, res, next) {
+  var item = {
+    Library_Name: req.body.inputLibName,
+    Library_Address: req.body.inputAddress
+    
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('Library').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/addLibrary');
+});
+
+router.post('/insertBook', function(req, res, next) {
+  var item = {
+    Book_Name: req.body.inputBookName,
+    Book_Author: req.body.inputBookAuthor
+    
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('Books').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/addBooks');
+});
+
+router.get('/listBooks', function(req, res, next) {
+  
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('Books').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('getBooks', {items: resultArray});
+    });
+  });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+router.get('/addLibrary', function(req, res, next) {
+  res.render('addLibrary');
+});
+
+router.get('/addBooks', function(req, res, next) {
+  res.render('addBooks');
+});
+router.get('/getBooks', function(req, res, next) {
+  res.render('getBooks');
+});
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+module.exports = router;
